0c2db6e43ad158488c3f7e1fbb00c281aa04b048
emkozuch <emkozuch@gmail.com>
2018-11-14 01:32:35 +0100

    zadanie 5

--- zadanie5/script.js
+++ zadanie5/script.js
@@ -1,14 +1,13 @@
-function displayRandom(){
-var addDiv = document.getElementById("square_container");
-for (i = 1; i <= 100; i++) {
-    var newDiv = document.createElement("div");
-    newDiv.id = "square" + i;
-    newDiv.className = "square";
-    newDiv.style.position = "absolute";
-    newDiv.style.left = Math.floor(Math.random() * 101) + "%";
-    newDiv.style.top = Math.floor(Math.random() * 101) + "%";
-    addDiv.appendChild(newDiv);
-}
-document.getElementById("square_container");
+function displayRandom() {
+    var addDiv = document.getElementById("square_container");
+    for (i = 1; i <= 100; i++) {
+        var newDiv = document.createElement("div");
+        newDiv.id = "square" + i;
+        newDiv.className = "square";
+        newDiv.style.position = "absolute";
+        newDiv.style.left = Math.floor(Math.random() * 101) + "%";
+        newDiv.style.top = Math.floor(Math.random() * 101) + "%";
+        addDiv.appendChild(newDiv);
+    }
 }
 displayRandom();
